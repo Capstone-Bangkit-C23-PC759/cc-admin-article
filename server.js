ad7df8a4a6e02f64ae64474f8d2bc1e099f88fd1
SyauqiMA <syauqiabrar@gmail.com>
2023-06-15 09:41:29 +0700

    modified server configuration

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const jwt = require('@hapi/jwt');
 
 const init = async () => {
 	const server = Hapi.server({
-		port: 3000,
+		port: process.env.PORT || 3000,
 		host: 'localhost',
+		routes: {
+			cors: true,
+		},
 	});
 
 	// Register jwt with the server
